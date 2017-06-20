9a2cd388a7ba5653e14f75e945342b9f829cfbad
Shawn Rider <shawn@shawnrider.com>
2017-06-20 10:35:55 -0700

    Initial files in place.
    Change-Id: I9c6dbe3662e50a262f3679b199050b3c8fdb7482

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# wats3020-sandwich-machine
-A JavaScript exercise to practice with Data Types and String manipulations.
+# WATS 3020 Sandwich Machine Assignment
+
+This project is meant to exercise working with different Data Types in
+JavaScript and using various techniques to combine text or convert data
+between types. You will be using a variety of techniques to take a sandwich
+order and produce a receipt for that order.
+
+In order to complete this assignment, it will be useful to read the section on
+[Data Types and Structures](https://shawnr.gitbooks.io/practical-introduction-to-javascript/content/data-types/)
+from _A Practical Introduction to JavaScript_.
+
+You may find these additional resources helpful for completing this project:
+
+* [Removing white space from Strings with `trim()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim)
+* [Use `toFixed()` to make numbers have two decimal places](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed)
+* [Using `split()` to break Strings into Arrays](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split)
+
+## Basic Requirements
+
+In general, you must complete all of the tasks spelled out in the `TODO` notes
+in the file `js/main.js` within this repository. You will write your JS code
+in that file, completing every task listed.
+
+* Prompt the user for information according to the tasks defined in `main.js`.
+* Process the input from the user to create useful Arrays, Strings, and other variables.
+* Reference the included `prices` Object in `main.js` to determine the price of the sandwich
+* Insert the order details and calculated price into the provided HTML structure in `index.html`.
+
+## Stretch Goals
+
+If the Basic Requirements are easy enough to accmoplish, then you are encouraged
+to attempt some more complex tasks, too. These are not spelled out in `main.js`,
+so you will have to figure out how to get them into your code on your own.
+
+* Implement some kind of special deal logic that provides a discount to the user. (First two toppings at X price; 20% discount on toppings if you order more than three toppings,  etc.)
+* Implement a more complex discount that prompts the user with something like: "You've added two toppings, so you qualify for a free third topping! What would you like to add to your sandwich?" or "Would you like to double your meat today?" Be sure to include that additional information in your output of the order and adjust your price according to whatever your discount concept is.
+* Add a line across the bottom of the receipt to say "Suggested Tips" and then calculate the tips for 10%, 15%, and 20% and provide those amounts for the convenience of the customer.
+
+
---
+++ js/main.js
@@ -0,0 +1,102 @@
+/* JavaScript for WATS 3020 Sandwich Machine Assignment */
+
+// Step One ///////////////////////////////////////////////////////////
+//
+// Gather data from the user using the prompt() command.
+// Users will fill in each prompt with text.
+// Provide examples where it may help (e.g. show a comma-separated list of
+// toppings to indicate how to specify toppings, etc.)
+//
+// TODO: Prompt the user for what kind of bread they would like.
+// Ideally, that would look something like: "What kind of bread (white, wheat, flat)?"
+
+
+
+// TODO: Prompt the user for what kind of meat(s) they would like.
+// Indicate they should separate multiple items with a comma:
+// "What kind of meat? (Separate meats with a comma if you would like more than one.)"
+
+
+
+// TODO: Prompt the user for what kind of toppings they would like.
+// We expect this to be multiple, so ask them to provide you with a
+// comma-separated list using a user friendly prompt.
+
+
+
+// TODO: Prompt the user for what kind of condiments they would like.
+// Again, we should expect a comma-separated list if items here.
+
+
+
+// Step Two ////////////////////////////////////////////////////////////
+//
+// Process all the input to calculate values for the user's order. The named
+// values are initialized to `null`. Change that so they equal the proper value.
+//
+// The `prices` object below specifies the prices for each thing.
+
+let prices = {
+    sandwich: 5, // Base price for a sandwich is $5, includes bread
+    meat: 1, // Each kind of meat on a sandwich costs $1
+    topping: 0.5, // Each topping costs $0.50
+    condiment: 0.25 // Each condiment costs $0.25
+};
+
+// TODO: Convert order information from Strings to Arrays.
+
+let meatArray = null;
+let toppingArray = null;
+let condimentArray = null;
+
+// TODO: Calculate cost for meat, toppings, and condiments.
+// This requires you to determine the length of each Array you just made
+// and multiply out the costs. You will need to refer to the attributes of the
+// `prices` object in order to calculate these costs.
+
+let meatCost = null;
+let toppingCost = null;
+let condimentCost = null;
+
+// TODO: Combine the costs of each part of the sandwich to get the subtotal.
+let subtotal = null;
+
+// TODO: Calculate the tax owed using the waStateTaxRate.
+let waStateTaxRate = 0.065;
+let orderTax = null;
+
+// TODO: Calculate `totalPrice` by adding `subtotal` and `orderTax`.
+let totalPrice = null;
+
+
+// Step Three //////////////////////////////////////////////////////////
+//
+// TODO: Now that we've calculated all the values, insert them into this
+// template literal using proper expression tags. Note that we must provide
+// all of the info the user gave us to confirm the order, and then we must also
+// provide the cost information so the user understands their bill.
+
+let receiptTemplate = `
+    <p>SANDWICH ORDER</p>
+    <p>Bread: wheat</p>
+    <p>Cheese: swiss</p>
+    <p>Meat: ham, turkey</p>
+    <p>Toppings: lettuce, tomato, peppers, spinach</p>
+    <p>Condiments: mayo, mustard, thousand island</p>
+    <p>---------------------</p>
+    <p class="text-right">Sandwich: $4.42</p>
+    <p class="text-right">Meat: $2.00</p>
+    <p class="text-right">Toppings: $2.00</p>
+    <p class="text-right">Condiments: $1.42</p>
+    <p class="text-right">--------</p>
+    <p class="text-right">Subtotal: $9.84</p>
+    <p class="text-right">Tax: $1.42</p>
+    <p class="text-right">--------</p>
+    <p class="text-right">Total: $4.84</p>
+`
+
+///////////////////////////////////////////////////////////////////////
+// Do not edit below this line unless you are doing something fancy!
+//////////////////////////////////////////////////////////////////////
+let receiptText = document.querySelector("#receipt-text");
+receiptText.innerHTML = receiptTemplate;
