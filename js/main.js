a4c9f56cceb4ecc4fe04520aa230ef06133370bf
vpangng <vpangng@gmail.com>
2018-04-15 16:14:21 -0400

    further modified main.js file and cleaned code

--- js/main.js
+++ js/main.js
@@ -1,41 +1,16 @@
 /* JavaScript for WATS 3020 Sandwich Machine Assignment */
 
-// Step One ///////////////////////////////////////////////////////////
-//
-// Gather data from the user using the prompt() command.
-// Users will fill in each prompt with text.
-// Provide examples where it may help (e.g. show a comma-separated list of
-// toppings to indicate how to specify toppings, etc.)
-//
-// TODO: Prompt the user for what kind of bread they would like.
-// Ideally, that would look something like: "What kind of bread (white, wheat, flat)?"
+//Prompt user for type of bread, meat, topping, and condiment
+
 let breadType = prompt('What kind of bread do you like? (white, wheat, flat)');
-
-
-// TODO: Prompt the user for what kind of meat(s) they would like.
-// Indicate they should separate multiple items with a comma:
-// "What kind of meat? (Separate meats with a comma if you would like more than one.)"
 
 let meatType = prompt('What kind of meat(s) do you like (separate meats with commas if including more than one)');
 
-
-// TODO: Prompt the user for what kind of toppings they would like.
-// We expect this to be multiple, so ask them to provide you with a
-// comma-separated list using a user friendly prompt.
-
 let toppingType = prompt('What kind of topping(s) do you like (separate toppings with commas if including more than one)');
-
-// TODO: Prompt the user for what kind of condiments they would like.
-// Again, we should expect a comma-separated list if items here.
 
 let condimentType = prompt('What kind of condiment(s) do you like (separate condiments with commas if including more than one)');
 
-// Step Two ////////////////////////////////////////////////////////////
-//
-// Process all the input to calculate values for the user's order. The named
-// values are initialized to `null`. Change that so they equal the proper value.
-//
-// The `prices` object below specifies the prices for each thing.
+//Calculate cost based on user input 
 
 let prices = {
     sandwich: 5, // Base price for a sandwich is $5, includes bread
@@ -44,38 +19,23 @@ let prices = {
     condiment: 0.25 // Each condiment costs $0.25
 };
 
-// TODO: Convert order information from Strings to Arrays.
-
 let meatArray = meatType.split(',');
 let toppingArray = toppingType.split(',');
 let condimentArray = condimentType.split(',');
 
-// TODO: Calculate cost for meat, toppings, and condiments.
-// This requires you to determine the length of each Array you just made
-// and multiply out the costs. You will need to refer to the attributes of the
-// `prices` object in order to calculate these costs.
 
 let meatCost = meatArray.length * prices.meat;
 let toppingCost = toppingArray.length * prices.topping;
 let condimentCost = condimentType.length * prices.condiment;
 
-// TODO: Combine the costs of each part of the sandwich to get the subtotal.
 let subtotal = meatCost + toppingCost + condimentCost + prices.sandwich;
 
-// TODO: Calculate the tax owed using the waStateTaxRate.
 let waStateTaxRate = 0.065;
 let orderTax = subtotal * 0.065;
 
-// TODO: Calculate `totalPrice` by adding `subtotal` and `orderTax`.
 let totalPrice = subtotal + orderTax;
 
-
-// Step Three //////////////////////////////////////////////////////////
-//
-// TODO: Now that we've calculated all the values, insert them into this
-// template literal using proper expression tags. Note that we must provide
-// all of the info the user gave us to confirm the order, and then we must also
-// provide the cost information so the user understands their bill.
+//Display receipt to user based on calculations above
 
 let receiptTemplate = `
     <p>SANDWICH ORDER</p>
